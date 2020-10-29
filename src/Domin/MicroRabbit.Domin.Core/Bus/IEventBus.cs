12454572769d22521fdda0263364dffa62321551
Sachin Kumar <sachinengineer@hotmail.com>
2020-10-29 22:47:46 +0530

    Initial Microservice structure

---
+++ src/Domin/MicroRabbit.Domin.Core/Bus/IEventBus.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Domin.Core.Commands;
+using MicroRabbit.Domin.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domin.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T Command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ src/Domin/MicroRabbit.Domin.Core/Bus/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domin.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domin.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent:Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler { }
+}
